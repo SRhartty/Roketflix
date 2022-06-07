4ad7bd3c7d186af11399f3c64ef9668d98429c83
thierre tries <randonsspace@gmail.com>
2022-06-07 11:02:02 -0300

    consultando

--- main.js
+++ main.js
@@ -4,19 +4,16 @@ function random() {
   var random = Math.random() * 5000;
   return Math.round(random);
 
-
 }
-
 
 const filme = (random())
 var url = BASE_URL + filme +'?api_key='+ API_KEY
-
-
 
 function getFilme() {
   axios.get(url)
     .then(response => {
       const data = response.data
+      teste.textContent = JSON.stringify(data)
 
     })
     .catch(error => console.log(error))
